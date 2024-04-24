46b478e13720e6478f96db7184a7e274caed1693
sonnie36 <sonniemwati36@gmail.com>
2024-04-24 08:55:05 -0700

    Added a function to check for palindrome

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -2,11 +2,21 @@ const CheckBtn = document.getElementById('check-btn');
 const textInput = document.getElementById('text-input');
 const result = document.getElementById('result');
 
+
+
 const checkPalindrome =()=>{
+
+    if (textInput.value === ""){
+        alert("Please input a value");
+        return
+    }
     const text = textInput.value;
     const reverseText = text.split('').reverse().join('');
-    if(text === null){
-        result.textContent = "Please input a value"
+    if(text===reverseText){
+        result.textContent = `${text} is a palindrome`;
     }
-    
+    else{
+        result.textContent = `${text} is not a palindrome`;
+    }
 }
+CheckBtn.addEventListener('click', checkPalindrome);
